a154eafe929cb31a8a5ceaf30012f5a55e18c2fd
SnowyField1906 <aswdqe1x@gmail.com>
2024-03-23 08:24:19 +0700

    Refactor MDX component styles and add roadmap page

--- components/MDX/Properties.tsx
+++ components/MDX/Properties.tsx
@@ -3,7 +3,7 @@ const Properties = ({ children }) => {
 		<div className="my-6">
 			<ul
 				role="list"
-				className="m-0 max-w-[calc(theme(maxWidth.lg)-theme(spacing.8))] list-none divide-y divide-zinc-900/5 p-0 dark:divide-white/5"
+				className="m-0 list-none divide-y divide-zinc-900/5 p-0 dark:divide-white/5"
 			>
 				{children}
 			</ul>
--- components/MDX/Property.tsx
+++ components/MDX/Property.tsx
@@ -4,14 +4,15 @@ const Property = ({ name, type, children }) => {
 			<dl className="m-0 flex flex-wrap items-center gap-x-3 gap-y-2">
 				<dt className="sr-only">Name</dt>
 				<dd>
-					<code>{name}</code>
+          <p className="text-lg font-bold text-zinc-600 dark:text-zinc-400">
+            {name}</p>
 				</dd>
 				<dt className="sr-only">Type</dt>
 				<dd className="font-mono text-xs text-zinc-400 dark:text-zinc-500">
 					{type}
 				</dd>
 				<dt className="sr-only">Description</dt>
-				<dd className="w-full flex-none [&>:first-child]:mt-0 [&>:last-child]:mb-0">
+				<dd className="w-full flex-none [&>:first-child]:mt-0 [&>:last-child]:mb-0 text-zinc-600 dark:text-zinc-400">
 					{children}
 				</dd>
 			</dl>
